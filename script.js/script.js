f86dc3b6702873bce77428a22e5bcc492f5abe3e
AdjeiMensah <48394224+AdjeiMensah@users.noreply.github.com>
2023-04-12 10:57:00 +0200

    improve folder structure

--- script.js/script.js
+++ script.js/script.js
@@ -1,30 +1,42 @@
-$(function() {
+function newItem() {
+  // Select the list element 
+  let list = $('#list');
+
+  // Define list item element
+  let li = $('<li></li>');
+
+  // Get value from the input
+  let inputValue = $('#input').val();
+
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    // Add input value to list item
+    li.append(inputValue);
     // Add new item to list
-    $('#add-button').on('click', function() {
-      let inputValue = $('#input').val();
-      if (inputValue === '') {
-        alert("You must write something!");
-      } else {
-        $('<li>').text(inputValue).appendTo('#list');
-        $('#input').val('');
-      }
-    });
-  
-    // Cross out item
-    $('#list').on('dblclick', 'li', function() {
-      $(this).toggleClass('strike');
-    });
-  
-    // Add delete button
-    $('#list').on('click', 'li', function() {
-      $('<button>').text('X').appendTo(this).on('click', function() {
-        $(this).parent().addClass('delete');
-      });
-    });
-  
-    // Make list sortable
-    $('#list').sortable({
-      animation: 150,
-    });
+    list.append(li);
+
+    // Set the input to empty
+    $('#input').val('');
+  }
+
+  // Cross out item
+  li.on('dblclick', function () {
+    $(this).toggleClass('strike');
   });
-  
+
+  // Add delete button
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'))
+  crossOutButton.on('click', function () {
+    li.addClass('delete');
+  });
+
+  // Add delete button to the list item
+  li.append(crossOutButton);
+
+  // Make list sortable
+  $('#list').sortable({
+    animation: 150,
+  });
+};
